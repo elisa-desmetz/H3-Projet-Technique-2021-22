fc93ed2b1147ec82102e87148e24530b6c5da134
elisa-desmetz <71330692+elisa-desmetz@users.noreply.github.com>
2021-09-08 16:44:05 +0200

    Add files via upload

---
+++ backend/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return "Service is running..."
